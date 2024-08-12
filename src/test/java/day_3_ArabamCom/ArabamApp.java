593295e746b6b2705606bee1cf9abd3cb4ebe51b
Nevzat <nevzatcelik151907@gmail.com>
2024-08-12 21:47:07 +0200

    first lesson

--- src/test/java/day_3_ArabamCom/ArabamApp.java
+++ src/test/java/day_3_ArabamCom/ArabamApp.java
@@ -22,7 +22,12 @@ public class ArabamApp {
         cap.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android"); // cihazin ios android neyse
         cap.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0"); // cihazin isletim sistemi
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2"); // cihazin otomasyonu
-        cap.setCapability(MobileCapabilityType.APP,"");
+        cap.setCapability("appPackage","com.dogan.arabam");
+        // AppPackage uzerinde caliscak oldugumuz uygulamanin kimlik bilgisidir. Apk infodan kimlik bilgisi bulunur
+        cap.setCapability("appActivity","com.dogan.arabam.presentation.feature.home.HomeActivity");
+        // AppActivity ise uzerinde calisacak oldugumuz uygulamanin hangi sayfasindan baslamak istedigimizi belir
+        // yani profil kismindan baslamak istiyorsam ilgili activity bulunur ve appActivity bolumune
+        // yazilarak uygulama oradan baslatilir
 
         driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
